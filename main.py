2f110f98e0d4c0e60a1cb0693db46c737ac4b31c
Leo Martinez III <123003509+LeoMartinezTAMUK@users.noreply.github.com>
2025-06-10 16:31:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def add(num1, num2):
+  num1 * num2 = num3
+  return num3
+
+ans = num3 * 6
+
+# The answer should be the first two numbers added together and then that sum gets multiplied by 5 (as stated in the description)
+print(ans)
